fb3595d422156f0c70ddaa709efd3a7ac04a79cd
josesanchez2004 <jsanc618@mtroyal.ca>
2024-11-08 22:12:31 -0700

    endpoints finished

--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,27 @@ module.exports.register = (app, database) => {
         res.status(200).send(JSON.stringify(records)).end();
     });
 
+    app.get('/api/resources/:id', async (req, res) => {
+        const employeeID = req.params.id;
+        database.query('SELECT * FROM rest_emp WHERE id = ?', [employeeID], (err,res) => {
+                res.json(result[0]);
+        })
+    });
+
+    app.get('api/resources/emp', (req,res) => {
+        const employeeName = req.query.name;
+        database.query('SELECT * FROM rest_emp WHERE name = ?', [employeeName], (res) => {
+                res.json(results);
+        })
+    })
+
+    app.post("api/emp", (req,res) => {
+        const {id,name,phone,email,address} = req.body;
+        const newEmployee = {id,name,phone,email,address};
+
+        database.query("INSERT INTO rest_emp SET ?", newEmployee);
+    })
+
+    
+
 };
